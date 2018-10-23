cb96d3e8cdd4c3370102b99799354289a7b4822e
dhawalshah11 <ds792@njit.edu>
2018-10-23 16:45:52 -0400

    Thank you Message Displayed

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/contact', 'PagesController@contact' )->name('contact');
 
 Route::post('/contact', 'PagesController@store' )->name('contact.store');
 
+Route::get('/thankyou/{name}', 'PagesController@thankyou' )->name('thankyou');
 
