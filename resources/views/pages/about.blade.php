9660f3a6f782be0020b0a5b517ebad1348a88d25
dhawalshah11 <ds792@njit.edu>
2018-10-23 03:25:39 -0400

    Home/About Page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+
+    <h1>¡Hola a todos! Bienvenidos a mi segunda página de inicio</h1>
+
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+
+    <h1>¡Hola a todos! Bienvenido a mi página de inicio</h1>
+
+
+@stop
